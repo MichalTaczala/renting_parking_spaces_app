bcc9fdfcee544bc01442bf0df83e24a8022dadfa
MichalTaczala <michtaczala@gmail.com>
2024-04-26 22:55:11 +0200

    updated terraform

--- terraform_conf/versions.tf
+++ terraform_conf/versions.tf
@@ -1,9 +1,15 @@
 terraform {
+  required_version = ">= 0.12"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 4.0"
     }
   }
-  required_version = ">= 0.12"
+
+  backend "gcs" {
+    bucket = "remote-terraform-files"
+    prefix = "terraform/state"
+  }
 }
