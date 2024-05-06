f5de008af1b86c796c60dcd3c79c8dbac68b9bdd
MichalTaczala <michtaczala@gmail.com>
2024-05-06 23:07:35 +0200

    flask add payment handling

--- backend_flask/main.py
+++ backend_flask/main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+import requests
 
 app = Flask(__name__)
 
@@ -6,6 +8,28 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/payment", methods=["POST"])
+def payment():
+    if request.is_json:
+        data = request.get_json()  # Get JSON data from the request
+        client_secret = data.get("client_secret")
+    response = requests.post(
+        "https://go-service-dot-parking-app-v2.uc.r.appspot.com/handlePayments",
+        json={"client_secret": client_secret},
+    )
+
+    if response.status_code != 200:
+        return (
+            jsonify({"message": "ERRORIK"}),
+            400,
+        )
+
+    # do changes in db
+    # TODO
+
+    return response.text
 
 
 if __name__ == "__main__":
