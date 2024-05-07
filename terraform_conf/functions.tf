aed1cf40646047e52d272151bf8073af7473b542
MichalTaczala <michtaczala@gmail.com>
2024-05-07 12:57:49 +0200

    fix terraform

--- terraform_conf/functions.tf
+++ terraform_conf/functions.tf
@@ -1,11 +1,11 @@
 resource "google_cloudfunctions_function" "function" {
-  name        = "function-test"
-  description = "My function"
+  name        = "payment_handling"
+  description = "payment handling function"
   runtime     = "python39"
 
   available_memory_mb          = 128
-  source_archive_bucket        = google_storage_bucket.backend_code.name
-  source_archive_object        = google_storage_bucket_object.function_source.name
+  source_archive_bucket        = google_storage_bucket.backend_bucket.name
+  source_archive_object        = google_storage_bucket_object.stripe_files.name
   trigger_http                 = true
   https_trigger_security_level = "SECURE_ALWAYS"
   timeout                      = 60
