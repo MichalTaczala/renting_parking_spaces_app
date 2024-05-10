48cf122cad7d5cffee8c5061e39cdfd1cc0cb163
MichalTaczala <michtaczala@gmail.com>
2024-05-10 10:35:47 +0200

    upload latest flask code

--- backend_flask/main.py
+++ backend_flask/main.py
@@ -12,12 +12,20 @@ def hello():
 
 @app.route("/payment", methods=["POST"])
 def payment():
-    if request.is_json:
-        data = request.get_json()  # Get JSON data from the request
-        client_secret = data.get("client_secret")
+    if not request.is_json:
+        return jsonify({"message": "Missing JSON in request"}), 400
+
+    data = request.get_json()  # Get JSON data from the request
+    client_secret = data.get("client_secret")
     response = requests.post(
-        "https://us-central1-parking-app-v2.cloudfunctions.net/payment_handling",
+        # "https://us-central1-parking-app-v2.cloudfunctions.net/payment_handling",
+        "http://127.0.0.1:8080/",
         json={"client_secret": client_secret},
+        headers={
+            # "Authorization": f"bearer {get_identity_token()}",
+            "Content-Type": "application/json",
+        },
+        timeout=70,
     )
 
     if response.status_code != 200:
