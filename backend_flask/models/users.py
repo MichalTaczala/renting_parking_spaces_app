9943af43f95c537aab89bcb145618429c895bd5a
Bartosz Grabek <bartoszgrabek7@gmail.com>
2024-05-11 17:47:14 +0200

    feat: add user model

---
+++ backend_flask/models/users.py
@@ -0,0 +1,23 @@
+# Description: This file contains the User model.
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Enum
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    username = sqlalchemy.Column(sqlalchemy.String(31), nullable=False)
+    user_name = sqlalchemy.Column(sqlalchemy.String(50), nullable=False)
+    user_surname = sqlalchemy.Column(sqlalchemy.String(100), nullable=False)
+    email = sqlalchemy.Column(sqlalchemy.String(100), nullable=False, unique=True)
+    user_type = sqlalchemy.Column(
+        Enum("regular", "admin", "guest", name="user_types"), default="regular"
+    )
+    phone_prefix = sqlalchemy.Column(sqlalchemy.String(3))
+    phone = sqlalchemy.Column(sqlalchemy.String(15), nullable=False, unique=True)
+    password = sqlalchemy.Column(sqlalchemy.String(50), nullable=False)
