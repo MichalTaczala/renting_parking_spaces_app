956f5699ce92ec13ffa1ba7367ab0bda32ddab01
wpartycja <w512patrycja@gmail.com>
2024-05-23 15:36:26 +0200

    feature: get booking

--- backend_flask/blueprints/booking/booking.py
+++ backend_flask/blueprints/booking/booking.py
@@ -37,3 +37,31 @@ def create_booking():
             return jsonify({"message": "Successfully created new booking!"}), 200
         except Exception as e:
             return jsonify({"message": str(e)}), 400
+
+
+@booking_bp.route("/booking/<int:booking_id>", methods=["GET"])
+def get_booking(booking_id):
+    """Endpoint for retrieving booking information."""
+    # Retrieve booking from the database based on booking_id
+    with get_session() as session:
+        try:
+            booking = session.query(Booking).filter(Booking.booking_id == booking_id).first()
+            if booking:
+                # Return booking information
+                return (
+                    jsonify(
+                        {
+                            "booking_start": booking.booking_start,
+                            "booking_end": booking.booking_end,
+                            "status": booking.status,
+                            "price_total": booking.price_total,
+                            "user_id": booking.user_id,
+                            "offer_id": booking.offer_id,
+                        }
+                    ),
+                    200,
+                )
+            else:
+                return jsonify({"message": "Booking not found"}), 404
+        except SQLAlchemyError as e:
+            return jsonify({"message": str(e)}), 500
